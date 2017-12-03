0d98588e2f8cf46b7b078bd4c6cd4ac865e45b5e
Toshiaki Ito <evolution@1024kb.cx>
2017-12-03 17:06:26 +0900

    add list view and adapter.
    resolve #11

--- app/src/main/java/cx/mb/mybarcodereader/presentation/main/MainActivityPresenterImpl.java
+++ app/src/main/java/cx/mb/mybarcodereader/presentation/main/MainActivityPresenterImpl.java
@@ -1,7 +1,12 @@
 package cx.mb.mybarcodereader.presentation.main;
 
 import android.app.Activity;
+import android.os.ResultReceiver;
 import cx.mb.mybarcodereader.presentation.barcode.BarcodeActivity;
+import cx.mb.mybarcodereader.realm.BarcodeRealm;
+import io.realm.Realm;
+import io.realm.RealmResults;
+import io.realm.Sort;
 
 /**
  * Presenter class for MainActivity.
@@ -11,12 +16,29 @@ public class MainActivityPresenterImpl implements MainActivityPresenter {
     /**
      * parent.
      */
-    private Activity parent;
+    private MainActivity parent;
+
+    /**
+     * Realm.
+     */
+    private Realm realm;
 
     @Override
     public void onCreate(Activity parent) {
-        this.parent = parent;
+        this.parent = (MainActivity) parent;
+        realm = Realm.getDefaultInstance();
+
+        final RealmResults<BarcodeRealm> items = realm.where(BarcodeRealm.class)
+                .findAllSorted("createAt", Sort.DESCENDING);
+        final ResultListAdapter adapter = new ResultListAdapter(items);
+        ((MainActivity) parent).resultList.setAdapter(adapter);
     }
+
+    @Override
+    public void onDestroy() {
+        realm.close();
+    }
+
 
     @Override
     public void startCamera() {
