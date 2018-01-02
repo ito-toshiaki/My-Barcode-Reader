6033a8b4f2f12119bd179e4ba8817eb0b3501ecd
Toshiaki Ito <evolution@1024kb.cx>
2018-01-02 19:54:03 +0900

    implement long click event.
    resolve #17

--- app/src/main/java/cx/mb/mybarcodereader/presentation/main/MainActivityPresenterImpl.java
+++ app/src/main/java/cx/mb/mybarcodereader/presentation/main/MainActivityPresenterImpl.java
@@ -1,15 +1,21 @@
 package cx.mb.mybarcodereader.presentation.main;
 
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
 
 import cx.mb.mybarcodereader.adapter.BarcodeListAdapter;
-import cx.mb.mybarcodereader.presentation.barcode.BarcodeActivity;
+import cx.mb.mybarcodereader.orma.Barcode;
 import cx.mb.mybarcodereader.orma.OrmaDatabase;
+import cx.mb.mybarcodereader.presentation.barcode.BarcodeActivity;
+import timber.log.Timber;
 
 /**
  * Presenter class for MainActivity.
  */
-public class MainActivityPresenterImpl implements MainActivityPresenter {
+public class MainActivityPresenterImpl implements MainActivityPresenter, AdapterView.OnItemLongClickListener {
 
     /**
      * parent.
@@ -22,6 +28,8 @@ public class MainActivityPresenterImpl implements MainActivityPresenter {
 
         BarcodeListAdapter adapter = new BarcodeListAdapter(this.parent, database.relationOfBarcode().orderByCreateAtDesc());
         ((MainActivity) parent).resultList.setAdapter(adapter);
+
+        ((MainActivity) parent).resultList.setOnItemLongClickListener(this);
     }
 
     @Override
@@ -34,4 +42,22 @@ public class MainActivityPresenterImpl implements MainActivityPresenter {
         parent.startActivity(BarcodeActivity.createIntent(parent));
     }
 
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+        final Barcode item = (Barcode) parent.resultList.getItemAtPosition(i);
+        if (item == null) {
+            Timber.w("item pos:%d is null.", i);
+            return true;
+        }
+
+        ClipData.Item clip = new ClipData.Item(item.getText());
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.setType("text/plain");
+        sendIntent.putExtra(Intent.EXTRA_TEXT, clip.getText());   // メモ帳のテキスト欄、メールアプリの本文にテキストをセット
+        parent.startActivity(sendIntent);
+
+        return true;
+    }
 }
